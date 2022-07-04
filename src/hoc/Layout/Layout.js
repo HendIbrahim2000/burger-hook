1469a8de811caac94e053a0a3b59375fdc094112
Hend Ibrahim <Hend.Ibrahim@Intellisec-me.com>
2022-07-04 11:32:52 +0200

    Changing HOC to hook

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux'
 
 import Auxel from '../Auxel/Auxel';
@@ -6,38 +6,35 @@ import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-class Layout extends Component {
-    state = {
-        showSideDrawer: false
+
+const Layout = props => {
+
+    const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false)
+
+    const sideDrawerClosedHandler = () => {
+        setSideDrawerIsVisible(false)
     }
 
-    sideDrawerClosedHandler = () => {
-        this.setState( { showSideDrawer: false } );
+    const sideDrawerToggleHandler = () => {
+        setSideDrawerIsVisible(!sideDrawerIsVisible)
     }
 
-    sideDrawerToggleHandler = () => {
-        this.setState( ( prevState ) => {
-            return { showSideDrawer: !prevState.showSideDrawer };
-        } );
-    }
-
-    render () {
         return (
             <Auxel>
                 <Toolbar
-                isAuth={this.props.isAuthenticated} 
-                drawerToggleClicked={this.sideDrawerToggleHandler} />
+                isAuth={props.isAuthenticated} 
+                drawerToggleClicked={sideDrawerToggleHandler} />
                 <SideDrawer
-                    isAuth={this.props.isAuthenticated}
-                    open={this.state.showSideDrawer}
-                    closed={this.sideDrawerClosedHandler} />
+                    isAuth={props.isAuthenticated}
+                    open={sideDrawerIsVisible}
+                    closed={sideDrawerClosedHandler} />
                 <main className={classes.Content}>
-                    {this.props.children}
+                    {props.children}
                 </main>
             </Auxel>
         )
+
     }
-}
 
 const mapStateToProps = state => {
     return {
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,46 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Auxel from '../Auxel/Auxel';
 
-const withErrorHandler = ( WrappedComponent, axios ) => {
-    return class extends Component {
-        state = {
-            error: null
-        }
 
-        componentWillMount () {
-            this.reqInterceptor = axios.interceptors.request.use( req => {
-                this.setState( { error: null } );
+
+const withErrorHandler = ( WrappedComponent, axios ) => {
+
+    return (props)=> {
+
+        const [error, setError] = useState(null)
+
+       
+            const reqInterceptor = axios.interceptors.request.use( req => {
+                setError(null)
                 return req;
             } );
-            this.resInterceptor = axios.interceptors.response.use( res => res, error => {
-                this.setState( { error: error } );
+            const resInterceptor = axios.interceptors.response.use( res => res, error => {
+                setError(error)
             } );
+       
+            useEffect(()=> {
+                return ()=> {
+                    axios.interceptors.request.eject( reqInterceptor );
+                    axios.interceptors.response.eject( resInterceptor );
+                }
+            }, [reqInterceptor, resInterceptor])
+
+
+        const errorConfirmedHandler = () => {
+            setError(null)
         }
 
-        componentWillUnmount () {
-            axios.interceptors.request.eject( this.reqInterceptor );
-            axios.interceptors.response.eject( this.resInterceptor );
-        }
-
-        errorConfirmedHandler = () => {
-            this.setState( { error: null } );
-        }
-
-        render () {
             return (
                 <Auxel>
                     <Modal
-                        show={this.state.error}
-                        modalClosed={this.errorConfirmedHandler}>
-                        {this.state.error ? this.state.error.message : null}
+                        show={error}
+                        modalClosed={errorConfirmedHandler}>
+                        {error ? error.message : null}
                     </Modal>
-                    <WrappedComponent {...this.props} />
+                    <WrappedComponent {...props} />
                 </Auxel>
             );
         }
     }
-}
 
 export default withErrorHandler;
