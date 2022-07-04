c597ad80c89e110d0fd3e18743c2332fe9f7cb71
Hend Ibrahim <Hend.Ibrahim@Intellisec-me.com>
2022-07-04 17:21:55 +0200

    memo and custom hooks

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,42 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Auxel from '../Auxel/Auxel';
+
+import useHttpErrorHandler from '../../hooks/http-error-handler'
 
 
 
 const withErrorHandler = ( WrappedComponent, axios ) => {
 
+
     return (props)=> {
+        const [error, errorHandler] = useHttpErrorHandler(axios)
 
-        const [error, setError] = useState(null)
-
-       
-            const reqInterceptor = axios.interceptors.request.use( req => {
-                setError(null)
-                return req;
-            } );
-            const resInterceptor = axios.interceptors.response.use( res => res, error => {
-                setError(error)
-            } );
-       
-            useEffect(()=> {
-                return ()=> {
-                    axios.interceptors.request.eject( reqInterceptor );
-                    axios.interceptors.response.eject( resInterceptor );
-                }
-            }, [reqInterceptor, resInterceptor])
-
-
-        const errorConfirmedHandler = () => {
-            setError(null)
-        }
+        
 
             return (
                 <Auxel>
                     <Modal
                         show={error}
-                        modalClosed={errorConfirmedHandler}>
+                        modalClosed={errorHandler}>
                         {error ? error.message : null}
                     </Modal>
                     <WrappedComponent {...props} />
